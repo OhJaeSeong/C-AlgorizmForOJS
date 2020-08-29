96dfd35e80f54d217303ca0eaabb78647478609f
OhJaeSeong <ojskih@naver.com>
2020-08-29 20:47:11 +0900

    백준 - 10988 팰린드롬인지 확인하기

---
+++ StringProcess/Palendrom.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CSharpAlgorizm.StringProcess
+{
+    class Palendrom
+    {//10988 팰린드롬
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int len = word.Length;
+            if(len == 1)
+            {
+                Console.WriteLine(1);
+            }
+            else
+            {
+                for(int n = 0; n < len/2; n++)
+                {
+                    if(word.Substring(n,1) != word.Substring(len - n - 1, 1))
+                    {
+                        Console.WriteLine(0);
+                        break;
+                    }
+                    if(n == (len/2)-1)
+                    {
+                        Console.WriteLine(1);
+                    }
+                }
+            }
+        }
+    }
+}
