1d253fe4c4658eafd76b85490cc72a37cff9bae1
OhJaeSeong <ojskih@naver.com>
2020-08-29 20:50:26 +0900

    백준 - 1543 문서 검색

---
+++ Searching/DocumentWord.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSharpAlgorizm.Searching
+{
+    class DocumentWord
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            int count = 0;
+            int len = b.Length; 
+            for (int x = 0; x < a.Length-b.Length+1; x++)
+            {
+                if(a.Substring(x, len) == b)
+                {
+                    count++;
+                    x += len - 1;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
