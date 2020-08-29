f5c7ceac52dfd91606d4f5ea25745951e83722e4
OhJaeSeong <ojskih@naver.com>
2020-08-29 20:27:23 +0900

    백준 - 2292 벌집

---
+++ Etc/beeHouse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CSharpAlgorizm
+{
+    class beeHouse
+    {
+        static void Main(string[] args)
+        {
+            string whatNum = Console.ReadLine();
+            int Num = Convert.ToInt32(whatNum);
+            int multiple = 0;
+            if (Num == 1)
+            {
+                Console.WriteLine(1);
+            }
+            else
+            {
+                Num -= 1;
+                while (Num > 0)
+                {
+                    multiple += 1;
+                    Num -= 6 * multiple;
+                }
+                Console.WriteLine(multiple+1);
+            }
+        }
+    }
+}
