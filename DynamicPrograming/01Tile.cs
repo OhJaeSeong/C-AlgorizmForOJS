1669a172e6f72902a8777ba4fc29e88e595fc2fa
OhJaeSeong <ojskih@naver.com>
2020-08-29 20:35:20 +0900

    백준 - 1904 01타일

---
+++ DynamicPrograming/01Tile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CSharpAlgorizm
+{
+    class _01Tile
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            long[] nCount = new long[n+1];
+            if(n == 0)
+            {
+                nCount[0] = 0;
+            }
+            else
+            {
+                nCount[0] = 1;
+                nCount[1] = 1;
+                if (n > 1)
+                {
+                    for (int k = 2; k < n + 1; k++)
+                    {
+                        nCount[k] = nCount[k - 2] + nCount[k - 1];
+                        nCount[k] = nCount[k] % 15746;
+                    }
+                }
+            }
+           
+            Console.WriteLine(nCount[n]);
+        }
+    }
+}
