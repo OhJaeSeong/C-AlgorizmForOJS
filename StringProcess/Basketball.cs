f71ac519be3f9406a5204b1206697c79115e9c1f
OhJaeSeong <ojskih@naver.com>
2020-08-29 20:44:23 +0900

    백준 - 1159 농구 경기

---
+++ StringProcess/Basketball.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpAlgorizm.StringProcess
+{
+    class Basketball
+    {
+        static void Main(string[] args)
+        {
+            int playerNumber = Convert.ToInt16(Console.ReadLine());
+            string players = "";
+            Dictionary<string, int> map = new Dictionary<string, int>();
+            for (int k = 0; k < playerNumber; k++)
+            {
+                players = Console.ReadLine();
+                if(!map.ContainsKey(players.Substring(0,1)))
+                {
+                    map.Add(players.Substring(0,1), 1);
+                }
+                else
+                {
+                    map[players.Substring(0, 1)] = map[players.Substring(0, 1)] + 1;
+                }
+            }
+            string list = "";
+            foreach (var i in map)
+            {
+                if(i.Value > 4)
+                {
+                    list += i.Key;
+                }
+            }
+            if (list.Equals(""))
+            {
+                Console.WriteLine("PREDAJA");
+            }
+            else
+            {
+                char[] ch = list.ToCharArray();
+                Array.Sort(ch);
+                Console.WriteLine(ch);
+            }
+        }
+    }
+}
