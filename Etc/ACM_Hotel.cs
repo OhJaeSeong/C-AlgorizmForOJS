44d90a23d1016c9ab38f40954959212d281c688a
OhJaeSeong <ojskih@naver.com>
2020-08-29 20:40:05 +0900

    백준 - 10250 ACM 호텔

---
+++ Etc/ACM_Hotel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace CSharpAlgorizm
+{
+    class ACM_Hotel
+    {
+        static void Main(string[] args)
+        {
+            string caseNum = Console.ReadLine();
+            int caseNumber = Convert.ToInt16(caseNum);
+            for (int c = 0; c < caseNumber; c++)
+            {
+                string[] testCase = Console.ReadLine().Split();
+                int h = int.Parse(testCase[0]);
+                int w = int.Parse(testCase[1]);
+                int n = int.Parse(testCase[2]);
+                int Place = 101; int people = 1;
+                for (int ww = 0; ww < w; ww++)
+                {
+                    for(int hh = 0; hh < h; hh++)
+                    {
+                        if(people == n)
+                        {
+                            Console.WriteLine(Place);
+                            break;
+                        }else if (hh == h-1)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            people++;
+                            Place += 100;
+                        }
+                    }
+                    if (people == n)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Place = Place - 100 * (h-1) + 1;
+                        people++;
+                    }
+                }
+            }
+        }
+    }
+}
